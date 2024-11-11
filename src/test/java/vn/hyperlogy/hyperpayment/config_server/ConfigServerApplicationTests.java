0eebc249302e7f480766338a889d72b36889e6a4
Hiếu Cao <91723664+hieu88coding@users.noreply.github.com>
2024-11-11 08:51:14 +0700

    initial

---
+++ src/test/java/vn/hyperlogy/hyperpayment/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hyperlogy.hyperpayment.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
